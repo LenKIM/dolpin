32d2a5cf2faa3902917f605c0f3d13b5218997da
LenKIM <joenggyu0@gmail.com>
2019-07-10 20:58:03 +0900

    feat: inital commit

---
+++ src/main/java/com/great/deploy/dolpin/DolpinApplication.java
@@ -0,0 +1,13 @@
+package com.great.deploy.dolpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolpinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolpinApplication.class, args);
+    }
+
+}
