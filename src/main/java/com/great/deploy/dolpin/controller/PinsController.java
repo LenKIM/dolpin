5894235d6d4583834e318df0cb3f93b45180942f
MisunJoo <0987someday@naver.com>
2019-08-02 18:51:23 +0900

    add : make delete pin API

--- src/main/java/com/great/deploy/dolpin/controller/PinsController.java
+++ src/main/java/com/great/deploy/dolpin/controller/PinsController.java
@@ -3,21 +3,25 @@ package com.great.deploy.dolpin.controller;
 import com.great.deploy.dolpin.dto.CelebrityResponse;
 import com.great.deploy.dolpin.dto.ClosePinResponse;
 import com.great.deploy.dolpin.dto.PinDetailResponse;
+import com.great.deploy.dolpin.dto.PinInfo;
 import com.great.deploy.dolpin.dto.PinResponse;
 import com.great.deploy.dolpin.dto.Response;
 import com.great.deploy.dolpin.service.PinService;
 import com.great.deploy.dolpin.service.s3.AmazonS3ClientService;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,5 +69,19 @@ public class PinsController {
   public Response<PinDetailResponse> getPinDetail(@PathVariable @NotBlank Long pinId){
     return new Response<>(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), pinService.getPinDetail(pinId));
   }
+
+//  /* 11. Pin 수정하기 API */
+//  @PutMapping("/pin/{pinId}")
+//  public Response<PinInfo> modifyPin(@PathVariable @NotBlank Long pinId, PinInfo pinInfo) {
+//    return new Response<>(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), pinService.modifyPin(pinId, pinInfo));
+//  }
+
+  /* 12. Pin 삭제하기 API */
+  @DeleteMapping("/pin/{pinId}")
+  public Response<String> deletePin(@PathVariable @NotBlank Long pinId) {
+    pinService.deletePin(pinId);
+    return new Response<>(HttpStatus.NO_CONTENT.value(), HttpStatus.NO_CONTENT.getReasonPhrase(), "true");
+  }
+
 
 }
