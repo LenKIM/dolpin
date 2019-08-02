55b26317ebaa969ef4a0aa4be91ca0926768c3de
LenKIM <joenggyu0@gmail.com>
2019-08-02 19:29:14 +0900

    left Dolpin Report

--- src/main/java/com/great/deploy/dolpin/controller/HelloWorldController.java
+++ src/main/java/com/great/deploy/dolpin/controller/HelloWorldController.java
@@ -1,11 +1,11 @@
 package com.great.deploy.dolpin.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Controller
+@RequestMapping("/api")
 public class HelloWorldController {
 
     @GetMapping("/")
