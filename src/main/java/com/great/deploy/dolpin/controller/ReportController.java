457f9f82b4bf6ad846536f826ce20d808282fd7f
LenKIM <joenggyu0@gmail.com>
2019-08-02 22:53:52 +0900

    fix: update swagger-ui

--- src/main/java/com/great/deploy/dolpin/controller/ReportController.java
+++ src/main/java/com/great/deploy/dolpin/controller/ReportController.java
@@ -5,29 +5,34 @@ import com.great.deploy.dolpin.dto.ProofResponse;
 import com.great.deploy.dolpin.dto.Response;
 import com.great.deploy.dolpin.exception.ResourceNotFoundException;
 import com.great.deploy.dolpin.service.ReportService;
+import com.great.deploy.dolpin.swagger.ProofResponseModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "ReportController", description = "Report 관련 API")
 @RestController
 @RequestMapping("/api/report")
 public class ReportController  {
 
-    private ReportService reportService;
-
+    @ApiOperation(value = "아이돌 저장하기", response = ProofResponseModel.class)
     @GetMapping("/proof/{pinId}")
     public Response<ProofResponse> reportProof(@PathVariable Long pinId) {
-         ProofResponse proofResponse = reportService.proof(new ProofRequest(pinId));
+        ProofResponse proofResponse = reportService.proof(new ProofRequest(pinId));
         if(!proofResponse.isResult()){
             throw new ResourceNotFoundException("pins", "id", "id");
         }
         return new Response<>(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), proofResponse);
     }
 
+    private ReportService reportService;
+
 //    @PostMapping("/proopf/dolpin")
-//    public Response<ProofResponse> reportDolpin(@RequestBody DolpinRequest dolpinRequest){
+//    public Response<ProofResponseModel> reportDolpin(@RequestBody DolpinRequest dolpinRequest){
 //        reportService.dolpin(dolpinRequest);
 //
 //    }
