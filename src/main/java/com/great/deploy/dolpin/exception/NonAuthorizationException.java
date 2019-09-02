d02badf05e0ca7ef483ec8b7ed43bdee9be5d070
LenKIM <joenggyu0@gmail.com>
2019-09-02 19:49:27 +0900

    feat: 예외 코드 추가

---
+++ src/main/java/com/great/deploy/dolpin/exception/NonAuthorizationException.java
@@ -0,0 +1,18 @@
+package com.great.deploy.dolpin.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class NonAuthorizationException extends DolpinException {
+
+    public NonAuthorizationException(int errorCode, String errorMsg) {
+        super(errorCode, errorMsg);
+    }
+
+    public int getErrorCode() {
+        return HttpStatus.UNAUTHORIZED.value();
+    }
+
+    public String getErrorMsg() {
+        return HttpStatus.UNAUTHORIZED.getReasonPhrase();
+    }
+}
