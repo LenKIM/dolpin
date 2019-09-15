3aeb91948a35887c03e9e42088e1631fca11a437
LenKIM <joenggyu0@gmail.com>
2019-09-15 18:41:40 +0900

    fix: update favorite

--- src/main/java/com/great/deploy/dolpin/domain/Favorite.java
+++ src/main/java/com/great/deploy/dolpin/domain/Favorite.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Embeddable;
 
+@Embeddable
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Favorite {
 
