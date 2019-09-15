4ce2716b9fe078cddfe16ac9964979147245d6df
LenKIM <joenggyu0@gmail.com>
2019-09-15 18:20:21 +0900

    fix: updatefavorite request param Set To Obejct

--- src/main/java/com/great/deploy/dolpin/controller/FavoriteController.java
+++ src/main/java/com/great/deploy/dolpin/controller/FavoriteController.java
@@ -3,10 +3,10 @@ package com.great.deploy.dolpin.controller;
 import com.great.deploy.dolpin.account.Account;
 import com.great.deploy.dolpin.account.CurrentUser;
 import com.great.deploy.dolpin.dto.AccountResponse;
+import com.great.deploy.dolpin.dto.FavoriteRequest;
 import com.great.deploy.dolpin.dto.FavoriteResponse;
 import com.great.deploy.dolpin.dto.Response;
 import com.great.deploy.dolpin.exception.BadRequestException;
-import com.great.deploy.dolpin.domain.Favorite;
 import com.great.deploy.dolpin.service.AccountService;
 import com.great.deploy.dolpin.swagger.FavoriteResponseModel;
 import io.swagger.annotations.Api;
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
-
-import java.util.Set;
 
 import static com.great.deploy.dolpin.account.Account.validateAccount;
 
@@ -37,14 +35,14 @@ public class FavoriteController {
 
     @PostMapping()
     @ApiOperation(value = "Post current user's favorites Info", response = FavoriteResponseModel.class)
-    public Response<AccountResponse> saveFavorites(@RequestBody Set<Favorite> favorites,
+    public Response<AccountResponse> saveFavorites(@RequestBody FavoriteRequest favorites,
                                                    @ApiIgnore @CurrentUser Account account) {
         validateAccount(account);
-        if (favorites.size() == 0) {
+        if (favorites.getFavorites().size() == 0) {
             throw new BadRequestException("No favorites in Request");
         }
 
-        Account savedAccount = Account.saveFavorites(account, favorites);
+        Account savedAccount = Account.saveFavorites(account, favorites.getFavorites());
 
         return new Response<>(
                 HttpStatus.ACCEPTED.value(),
--- src/main/java/com/great/deploy/dolpin/domain/Favorite.java
+++ src/main/java/com/great/deploy/dolpin/domain/Favorite.java
@@ -1,17 +1,14 @@
 package com.great.deploy.dolpin.domain;
 
 
-import com.great.deploy.dolpin.common.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Embeddable;
 
-@Embeddable
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Favorite extends AuditEntity {
+public class Favorite {
 
     private Long memberId;
     private Long groupId;
