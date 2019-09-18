9a143a33a78b5ae333e694d2f78bb31a11218fc3
LenKIM <joenggyu0@gmail.com>
2019-09-18 23:56:07 +0900

    feat: Pin 인증 API repo 추가

---
+++ src/main/java/com/great/deploy/dolpin/repository/VisitRepository.java
@@ -0,0 +1,8 @@
+package com.great.deploy.dolpin.repository;
+
+import com.great.deploy.dolpin.domain.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+}
