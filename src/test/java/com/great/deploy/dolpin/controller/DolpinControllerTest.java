12d1fbdcc8cc99e227afc233e706e3f6f730643c
LenKIM <joenggyu0@gmail.com>
2019-10-03 01:54:34 +0900

    chore: 제보 기능 테스트 코드 삽입

--- src/test/java/com/great/deploy/dolpin/controller/DolpinControllerTest.java
+++ src/test/java/com/great/deploy/dolpin/controller/DolpinControllerTest.java
@@ -5,6 +5,7 @@ import com.great.deploy.dolpin.domain.CelebrityGroup;
 import com.great.deploy.dolpin.domain.CelebrityMember;
 import com.great.deploy.dolpin.domain.Pins;
 import com.great.deploy.dolpin.dto.CreatePinRequest;
+import com.great.deploy.dolpin.dto.DolpinRequest;
 import com.great.deploy.dolpin.dto.ProofRequest;
 import com.great.deploy.dolpin.repository.CelebrityGroupRepository;
 import com.great.deploy.dolpin.repository.CelebrityMemberRepository;
@@ -13,6 +14,9 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
@@ -60,12 +64,12 @@ public class DolpinControllerTest extends BaseControllerTest {
         ProofRequest request = new ProofRequest(save.getId(), 3);
         this.mockMvc.perform(
                 post("/api/report/proof")
-                .header(HttpHeaders.AUTHORIZATION, super.getBearerToken(true))
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(this.objectMapper.writeValueAsString(request))
+                        .header(HttpHeaders.AUTHORIZATION, super.getBearerToken(true))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(request))
         )
                 .andExpect(MockMvcResultMatchers.jsonPath("code").value("200"))
-                .andExpect(MockMvcResultMatchers.jsonPath("msg").value("OK"));
+                .andExpect(MockMvcResultMatchers.jsonPath("msg").value("OKa"));
     }
 
     @Test
@@ -105,5 +109,63 @@ public class DolpinControllerTest extends BaseControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("code").value("401"))
                 .andExpect(MockMvcResultMatchers.jsonPath("msg").value("인증되지 않은 핀 정보입니다."))
                 .andExpect(MockMvcResultMatchers.jsonPath("data").exists());
+    }
+
+
+    @Test
+    public void 제보하기_성공() throws Exception {
+        DolpinRequest dolpinRequest = DolpinRequest.builder()
+                .address("서울시 강남구")
+                .detailedAddress("강남역 8번 출구")
+                .celebrityMemberId(1L)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.of(2019, 10, 30))
+                .build();
+
+        MockMultipartFile multipartFile = new MockMultipartFile("data", "김정규.jpg",
+                "image/jpeg", "SpringAAAAasdasdFramework".getBytes());
+
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders
+                        .multipart("/api/report/dolpin")
+                        .file(multipartFile)
+                        .header(HttpHeaders.AUTHORIZATION, super.getBearerToken(true))
+                        .param("model", this.objectMapper.writeValueAsString(dolpinRequest))
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("202"))
+                .andExpect(MockMvcResultMatchers.jsonPath("msg").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("data").value(true))
+        ;
+
+    }
+
+    @Test
+    public void 제보하기_null값실패() throws Exception {
+
+        DolpinRequest dolpinRequest = new DolpinRequest();
+
+        MockMultipartFile multipartFile = new MockMultipartFile("data", "김정규.jpg",
+                "image/jpeg", "TestContets".getBytes());
+
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders
+                        .multipart("/api/report/dolpin")
+                        .file(multipartFile)
+                        .header(HttpHeaders.AUTHORIZATION, super.getBearerToken(true))
+                        .param("model", this.objectMapper.writeValueAsString(dolpinRequest))
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("202"))
+                .andExpect(MockMvcResultMatchers.jsonPath("msg").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("data").value(false))
+        ;
+
     }
 }
